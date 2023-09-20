f09efd144b0c82226b43e11f1f7ab29c8d32baa5
Elton Moreno <eltonmorenocl@gmail.com>
2023-09-20 20:34:16 -0300

    feat: adiciona detalhes do pokemon ao clickar

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,14 +1,18 @@
 const pokemonList = document.getElementById('pokemonList')
 const loadMoreButton = document.getElementById('loadMoreButton')
+const returnButton = document.getElementById('returnButton');
+const pokemonDetail = document.querySelector('#pokemonDetail');
+const pokemons = document.querySelectorAll('.pokemons');
+
 
 const maxRecords = 151
-const limit = 10
+const limit = 9
 let offset = 0;
 
 function convertPokemonToLi(pokemon) {
     return `
         <li class="pokemon ${pokemon.type}">
-            <span class="number">#${pokemon.number}</span>
+            <span class="number">${pokemon.number}</span>
             <span class="name">${pokemon.name}</span>
 
             <div class="detail">
@@ -45,3 +49,63 @@ loadMoreButton.addEventListener('click', () => {
         loadPokemonItens(offset, limit)
     }
 })
+
+// Adicione um ouvinte de evento para cada elemento .pokemon
+pokemons.forEach((pokemon) => {
+    pokemon.addEventListener('mouseenter', () => {
+        // Quando o mouse entra no Pokémon, altere o estilo do cursor
+        pokemon.style.cursor = 'pointer'; // Altere para o estilo de cursor desejado
+    });
+
+    // Remova o estilo do cursor quando o mouse sai do Pokémon (evento mouseleave)
+    pokemon.addEventListener('mouseleave', () => {
+        pokemon.style.cursor = 'auto'; // Volte ao estilo de cursor padrão
+    });
+});
+
+returnButton.addEventListener('click', () => {
+    // Chame uma função para restaurar todas as <li> que foram removidas
+    restoreAllPokemonItems();
+    // Remova a <div class="pokemon-detail"> quando o botão "Return" for clicado
+    const pokemonDetail = document.querySelector('#pokemon-detail');
+    if (pokemonDetail) {
+        pokemonDetail.remove();
+    }
+    
+    // Exiba novamente o botão "Load More"
+    loadMoreButton.style.display = 'block';
+    // Oculte o botão "Return" novamente após clicar em "Return"
+    returnButton.style.display = 'none';
+});
+
+pokemonList.addEventListener('click', (event) => {
+    const clickedPokemon = event.target.closest('.pokemon');
+    if (clickedPokemon) {
+        // Remova todos os elementos <li> da lista
+        const pokemonList = document.getElementById('pokemonList');
+        while (pokemonList.firstChild) {
+            pokemonList.removeChild(pokemonList.firstChild);
+        }
+        // Oculte o botão "Load More"
+        loadMoreButton.style.display = 'none';
+
+        // Exiba o botão "Return" após clicar em um Pokémon
+        returnButton.style.display = 'block';
+
+        // Obtenha o número do Pokémon clicado
+        const pokemonNumber = clickedPokemon.querySelector('.number').textContent;
+
+        // Chame a API para obter os detalhes do Pokémon clicado
+        getPokemonDetails(pokemonNumber);
+    }
+});
+
+function restoreAllPokemonItems() {
+    // Aqui você deve adicionar novamente todas as <li> que foram removidas
+    // Usei a mesma lógica que você usou para carregar os Pokémon inicialmente
+    loadPokemonItens(offset, limit);
+}
+
+// Selecione todos os elementos com a classe .pokemon
+
+
--- assets/js/poke-api.js
+++ assets/js/poke-api.js
@@ -3,6 +3,7 @@ const pokeApi = {}
 
 function convertPokeApiDetailToPokemon(pokeDetail) {
     const pokemon = new Pokemon()
+
     pokemon.number = pokeDetail.id
     pokemon.name = pokeDetail.name
 
@@ -12,7 +13,7 @@ function convertPokeApiDetailToPokemon(pokeDetail) {
     pokemon.types = types
     pokemon.type = type
 
-    pokemon.photo = pokeDetail.sprites.other.dream_world.front_default
+    pokemon.photo = pokeDetail.sprites.versions["generation-v"]["black-white"].animated.front_default
 
     return pokemon
 }
@@ -33,3 +34,36 @@ pokeApi.getPokemons = (offset = 0, limit = 5) => {
         .then((detailRequests) => Promise.all(detailRequests))
         .then((pokemonsDetails) => pokemonsDetails)
 }
+
+function getPokemonDetails(pokemonNumber) {
+    const apiUrl = `https://pokeapi.co/api/v2/pokemon/${pokemonNumber}`;
+
+    fetch(apiUrl)
+        .then((response) => response.json())
+        .then((data) => {
+            // Verifique se 'data.types' está definido e se possui elementos
+            const type = data.types.length > 0 ? data.types[0].type.name : "Unknown";
+
+            // Crie uma div para exibir os detalhes do Pokémon
+            const pokemonDetailDiv = document.createElement('div');
+            pokemonDetailDiv.setAttribute('id', 'pokemon-detail');
+
+            // Preencha a div com informações de detalhes
+            pokemonDetailDiv.innerHTML = `
+                <h2>${data.name}</h2>
+                <p>Number: ${data.id}</p>
+                <p>Type: ${type}</p>
+                <img src="${data.sprites.versions["generation-v"]["black-white"].animated.front_default}" alt="${data.name}">
+            `;
+
+            pokemonDetailDiv.classList.add(type);
+
+            // Adicione a div de detalhes à seção de conteúdo
+            const contentSection = document.querySelector('.content');
+            contentSection.appendChild(pokemonDetailDiv);
+        })
+        .catch((error) => {
+            console.error('Erro ao buscar detalhes do Pokémon:', error);
+        });
+}
+
